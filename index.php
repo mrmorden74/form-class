449bcfbb3a63caa3eb45e367e5270f70f24ca900
mrmorden74 <HitzingerA@wifi-it.at>
2016-10-27 14:46:33 +0200

    First

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1); // php7 Muss die erste Anweisung in einer Datei sein
+require_once 'inc/Character.class.php';
+require_once 'inc/Sorcerer.class.php';
+?><!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Form-Class</title>
+    <link rel="stylesheet" href="css/pure-min.css">
+    <link rel="stylesheet" href="css/layout.css">
+</head>
+<body>
+    <div class="wrapper">
+        <header class="main-header">
+            <h1>Objekt orientiertes Programmieren</h1>
+        </header>
+        <main>
+    <?php
+
+    ?>
+        </main>
+    </div>
+</body>
+</html>
