1fc26c2a7c2a3f9c0746ec06cccd53ea2904d957
Hitzinger <HitzingerA@wifi-it.at>
2016-10-27 15:27:41 +0200

    First

---
+++ inc/contact-form-config.inc.php
@@ -0,0 +1,16 @@
+<?php
+$formConf = [
+	'form' => [
+		'method' => 'post',
+		'action' => '',
+		'enctype' => 'UTF8',
+		'tagAtributes' => [
+			'class' => 'pure-form pure-form-stacked',
+			'id' => 'contactForm' 
+		]
+	],
+	'fields' => [
+
+	]
+];
+?>
--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 <?php
 declare(strict_types=1); // php7 Muss die erste Anweisung in einer Datei sein
-require_once 'inc/Character.class.php';
-require_once 'inc/Sorcerer.class.php';
+// require_once 'inc/Character.class.php';
+// require_once 'inc/Sorcerer.class.php';
 ?><!DOCTYPE html>
 <html lang="de">
 <head>
     <meta charset="UTF-8">
-    <title>Form-Class</title>
+    <title>Php Form Klasse</title>
     <link rel="stylesheet" href="css/pure-min.css">
     <link rel="stylesheet" href="css/layout.css">
 </head>
 <body>
     <div class="wrapper">
         <header class="main-header">
-            <h1>Objekt orientiertes Programmieren</h1>
+            <h1>Formularrendering über Form Klasse</h1>
         </header>
         <main>
+        
     <?php
 
     ?>
